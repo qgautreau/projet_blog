4b6e0cec17e43dc7acacbfcf90682faf060d5f10
juandarov <juandarov@gmail.com>
2017-05-04 15:19:02 +0200

    my commit

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'post'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'post', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
